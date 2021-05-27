9c489fcab2802a6a894501f7ab8ff7c7eb7ba637
Harold Torres Marino <haroldtm55@gmail.com>
2021-05-27 18:22:28 +1000

    Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(userName,userEmail) {
+  const formData = {
+    name: userName,
+    email: userEmail
+  }
+  const configObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  }
+  return fetch('http://localhost:3000/users', configObj).then((response) => {
+    return response.json()
+  }).then((object) => {
+    console.log(object['name'])
+    document.body.appendChild(document.createElement('p')).innerHTML = object['id']
+  }).catch((error) => {
+    document.body.appendChild(document.createElement('p')).innerHTML = 'Unauthorized Access'
+  })
+}
